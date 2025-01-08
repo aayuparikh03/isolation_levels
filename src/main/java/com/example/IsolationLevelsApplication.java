6d58d7a29a0b5510b23631710aa15e3084a1ec61
Aayu Parikh <aryanparikh03@gmail.com>
2025-01-08 16:09:15 +0530

    Initial commit

---
+++ src/main/java/com/example/IsolationLevelsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsolationLevelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsolationLevelsApplication.class, args);
+	}
+
+}
